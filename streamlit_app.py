481ed148eb8932c685f294ebae5a859f9afcd732
Sabrina Bermudez-Riley <113366915+smbr15@users.noreply.github.com>
2023-08-03 13:08:05 -0500

    Update streamlit_app.py
    put fruityvice response to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,5 +26,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
 
+# this puts the fruityvice response in dataframe
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
