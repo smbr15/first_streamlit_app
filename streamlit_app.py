bcbcf5b926765fcd120de002371a1dfa869a327a
Sabrina Bermudez-Riley <113366915+smbr15@users.noreply.github.com>
2023-08-04 09:40:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,8 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
+streamlit.write('Thanks for adding ', add_my_fruit)
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
                                     
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
